683d5dc8c38daa3185f80c70500c48385cc2d3d5
이명제 <mjsj1218@naver.com>
2023-02-07 16:51:55 +0900

    programmers level2 6~9

---
+++ programmers/level2/programmers_2_6.js
@@ -0,0 +1,22 @@
+
+/** 숫자의 표현 */
+function solution(n) {
+    let answer = 0;
+    let sum = 0;
+    let ct = 0;
+    for(let i=1; i<=n; i++) {
+        sum+=i;
+        ct++;
+        if(sum===n) {
+            sum = 0;
+            i = i-ct+1;
+            ct = 0;
+            answer++;
+        } else if(sum>n){
+            sum = 0;
+            i = i-ct+1;
+            ct = 0;
+        }
+    }
+    return answer;
+}
---
+++ programmers/level2/programmers_2_7.js
@@ -0,0 +1,10 @@
+/** 피보나치 수 */
+function solution(n) {
+    const arr = [0, 1];
+    
+    for(let i=2; i<=n; i++) {
+        arr.push((arr[i-2] + arr[i-1])%1234567);
+    }
+    
+    return arr[arr.length-1];
+}
